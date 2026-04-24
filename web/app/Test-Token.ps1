647c87f8d48011d029e8b92afd7ce3c14c06e991
Shannon Bray | Parex Technology <shannon.bray@parextechnology.com>
2026-04-24 09:44:57 -0400

    fix: update allowed SharePoint origins for customer access

--- web/app/Test-Token.ps1
+++ web/app/Test-Token.ps1
@@ -1,5 +1,6 @@
 $accessToken = $env:LI_ACCESS_TOKEN   # store it in env or Key Vault, not inline
-$accessToken = "AQWybr0H6rOoCpW_U3rkTGf-t9vLXKsW0YsHO3_05BJ0YNgyttCc-7JRLtLcK5K0u_zp30hVy1j-aJAXXMpvN2W7_X7wd08P2HM8RAzlx2c72OBkOygcYSeBI0wXSLxiEiwNRRuW4zsXgPRww-ytyWDba8afCIpobKFjeriZm264SzQ2Sn-xxWOjR5IKzTZR7wCExjqf3dF2-SK4krvEX5arX5T9rLT0EQnkzo-Ry3HDe_EfPlDcl1i6r6G_gU9py4qF5NGtEDU-9kkXGVWyzWNHV-xskrcSZWkRuTy1NZMDcWcrICtJkUJRXzDRdlOTVtQX-mmQTz_dVKUzKyJakmSK7FW9VA"
+$accessToken = "AQXkZenWLjnjcY-w6O6BOvz0fDcAziy8TidvMKV_x8fNxp44FkTS0bupg0O-3jlCpXpFi2BAEaQeKxMw8ewykegk-5TY8wV7O0tB76hR0FoWpxs4vi4-5agn2J9Q9jpgOABhdYRm9FyXVf0ca5gTPO9KQA3IzZ9-xJTwGthORSsfwTw4qtyjiYg0Kn_bhtfoIJW0zSi1knTqDkdgh80nLB8CFC4dMFTnFWOgKMjRAniR6hv1WQTDwGwhg6vQdhSXnDUCjua1efkV7rOQImInLHb6D6jtzlJUabKgYOQbmPM7NxPL7GbrF3pgn1qA9zPvfae4rsTaNH8y127QX-ix8-U4_-uobQ"
+
 
 
 $orgId = "9462"                   # <-- your org id
@@ -12,11 +13,12 @@ $url = "https://api.linkedin.com/rest/posts?author=$authorEncoded&q=author&count
 $headers = @{
   "Authorization"              = "Bearer $accessToken"
   "X-Restli-Protocol-Version"  = "2.0.0"
-  "Linkedin-Version"           = "202601"     # YYYYMM format :contentReference[oaicite:2]{index=2}
+  "Linkedin-Version"           = "202602"     # YYYYMM format :contentReference[oaicite:2]{index=2}
   "X-RestLi-Method"            = "FINDER"
 }
 
 Write-Host "Calling URL:`n$url`n"
 
 Invoke-RestMethod -Method Get -Uri $url -Headers $headers
+
 
